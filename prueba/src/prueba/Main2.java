3fbc4758adee87cbc83091ad883f2e6369e37729
Gionwel <juangaloisor@gmail.com>
2025-09-23 14:03:15 +0200

    ...

---
+++ prueba/src/prueba/Main2.java
@@ -0,0 +1,22 @@
+package prueba;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		a = 12;
+		
+		
+		
+		int b = a + 1;
+		
+		
+		b = 5;
+		
+		System.out.println(b);
+
+	}
+
+}
