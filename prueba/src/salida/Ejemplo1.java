1ddf9eb0140148cbafed3cec08bdc8f17ff5f1cc
Gionwel <juangaloisor@gmail.com>
2025-09-24 14:40:46 +0200

    ...

---
+++ prueba/src/salida/Ejemplo1.java
@@ -0,0 +1,21 @@
+package salida;
+
+import java.util.Scanner;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		
+		Scanner pepe = new Scanner(System.in);
+		
+		Scanner nombre = pepe.next();
+		
+		System.out.printf("Hola %s \n", nombre);
+		
+		
+		
+		
+		
+	}
+
+}
